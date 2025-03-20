bb1ca0c074223f77c913784ea0a446ae71551a13
Jantopthegreat <fauzangriya@gmail.com>
2025-03-21 04:19:03 +0700

    creating statefulwidget for profilepage and class "Tugas"

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class Tugas {
+  String tugas;
+  DateTime deadline;
+  bool status;
+
+  Tugas({required this.tugas, required this.deadline, this.status = false});
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController tugasController = TextEditingController();
+  final key = GlobalKey<FormState>();
+  List<Tugas> daftarTugas = [];
+  DateTime? selectedDate;
+
+  
